7fa85f74e9ff81bc671ada1839135874c145af23
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-12-26 20:57:51 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,8 @@ function is_email(email){
 }
 else{
   form.innerHTML = `<br><br><br>
-  <h1>Obrigado(a) , ${nome.value}</h1>`
+  <h1>Obrigado(a) , ${nome.value}</h1>
+  <h2> Earth (C-137) - ${moment().format("MMM Do YY")}</h2>`
 }
 }
 
