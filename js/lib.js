eb3cec8b67ea220a954c7718a163932093db2231
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-12-26 19:56:24 -0300

    Update lib.js

--- js/lib.js
+++ js/lib.js
@@ -1,5 +1,45 @@
-export function efeitoScroll(){
-    console.log('A')
-}
-
-
+export function showdata(elements){
+    let result = ``
+    let list = elements["results"]
+      for(let i = 0; i<=11;i++){
+      if (list[i].status == 'Alive'){
+        cor = 'rgb(0, 197, 16);'
+      }
+      if (list[i].status == 'Dead'){
+        cor = 'rgb(221, 0, 0);'
+      }
+      if (list[i].status == 'unknown'){
+        cor = 'whitesmoke;'
+      }
+        let element =`
+        <div class="item"  style = "text-align: center;"><img src="${list[i].image}" alt="${list[i].name}">
+        <p style = "text-align: center;">${list[i].name}</p>
+        <p style = "color: ${cor};" style = "text-align: center;">${list[i].status}</p>
+        </div>
+        `
+        result+= element
+    }
+    item.innerHTML = result
+    text.addEventListener('change', function(){
+      result = ''
+      for(let i = 0; i<=11;i++){
+        if (list[i].name.includes(text.value)){
+          if (list[i].status == 'Alive'){
+            cor = 'rgb(0, 197, 16);'
+          }
+          if (list[i].status == 'Dead'){
+            cor = 'rgb(221, 0, 0);'
+          }
+          if (list[i].status == 'unknown'){
+            cor = 'whitesmoke;'
+          }
+          let element =`
+          <div class="item"  style = "text-align: center;"><img src="${list[i].image}" alt="${list[i].name}">
+          <p style = "text-align: center;">${list[i].name}</p>
+          <p style = "color: ${cor};" style = "text-align: center;">${list[i].status}</p>
+          </div>
+          `
+          result+= element
+        }}
+     item.innerHTML = result
+    })}
