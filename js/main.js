ca5b8a9824aa4f59fdcce42e6f4a59f5bea32b91
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-12-26 19:56:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-import {efeitoScroll} from './lib'
+import {showdata} from './lib.js';
 //API DOS PERSONAGENS
+let cor = ''
 let item = document.querySelector('.characters')
 let text = document.querySelector('input[id = "search-input"]')
 let botao = document.getElementsByClassName('.botaok')
@@ -8,54 +9,6 @@ function getC(link){
   .then(response => response.json())
   .then(elements => showdata(elements))
 }
-function showdata(elements){
-  let result = ``
-  let list = elements["results"]
-    for(let i = 0; i<=11;i++){
-    if (list[i].status == 'Alive'){
-      cor = 'rgb(0, 197, 16);'
-    }
-    if (list[i].status == 'Dead'){
-      cor = 'rgb(221, 0, 0);'
-    }
-    if (list[i].status == 'unknown'){
-      cor = 'whitesmoke;'
-    }
-      let element =`
-      <div class="item"  style = "text-align: center;"><img src="${list[i].image}" alt="${list[i].name}">
-      <p style = "text-align: center;">${list[i].name}</p>
-      <p style = "color: ${cor};" style = "text-align: center;">${list[i].status}</p>
-      </div>
-      `
-      result+= element
-  }
-  item.innerHTML = result
-  text.addEventListener('change', function(){
-    result = ''
-    for(let i = 0; i<=11;i++){
-      if (list[i].name.includes(text.value)){
-        if (list[i].status == 'Alive'){
-          cor = 'rgb(0, 197, 16);'
-        }
-        if (list[i].status == 'Dead'){
-          cor = 'rgb(221, 0, 0);'
-        }
-        if (list[i].status == 'unknown'){
-          cor = 'whitesmoke;'
-        }
-        let element =`
-        <div class="item"  style = "text-align: center;"><img src="${list[i].image}" alt="${list[i].name}">
-        <p style = "text-align: center;">${list[i].name}</p>
-        <p style = "color: ${cor};" style = "text-align: center;">${list[i].status}</p>
-        </div>
-        `
-        result+= element
-      }}
-   item.innerHTML = result
-  })}
-
-
-
 
 getC('https://rickandmortyapi.com/api/character/')
 
@@ -83,4 +36,3 @@ function is_email(email){
   }
 }
 
-window.addEventListener('scroll', efeitoScroll)
